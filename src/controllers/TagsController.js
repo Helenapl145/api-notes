5fa959fa1e70ca28718de218d87ac2ad445f7587
Helena <helenapl145@gmail.com>
2024-03-05 19:26:01 -0300

    update tagsController

--- src/controllers/TagsController.js
+++ src/controllers/TagsController.js
@@ -4,13 +4,12 @@ class TagsController {
     async index(request, response) {
         const user_id = request.user.id;
         const tags = await knex("tags")
-            .select('id', 'name') 
             .where({ user_id })
             .groupBy('id', 'name'); 
 
         return response.json(tags);
     }
-};
+}
 
 
 module.exports =  TagsController;
