76469e5b640d0c4b74f712334f9874bd45bbdc13
Helena <helenapl145@gmail.com>
2024-03-05 17:17:02 -0300

    update createNotes

--- src/database/knex/migrations/20231121175545_createNotes.js
+++ src/database/knex/migrations/20231121175545_createNotes.js
@@ -1,16 +1,21 @@
 exports.up = function(knex) {
-  return knex.schema.createTable("notes", table =>{
-    table.increments("id");
-
-    table.string("title");
-    table.text("description");
-    table.integer("user_id").references("id").inTable("users");
-    table.timestamp("created_at").defaultTo(knex.fn.now()); // Correção aqui
-    table.timestamp("updated_at").defaultTo(knex.fn.now()); // Correção aqui
-  })
-  .then(() => {
-    // Altera a colação da coluna 'title' após criar a tabela
-    return knex.schema.raw('ALTER TABLE notes MODIFY title VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;');
+  return knex.schema.hasTable('notes').then(function(exists) {
+    if (!exists) {
+      return knex.schema.createTable("notes", table => {
+        table.increments("id");
+        table.string("title");
+        table.text("description");
+        table.integer("user_id").references("id").inTable("users");
+        table.timestamp("created_at").defaultTo(knex.fn.now());
+        table.timestamp("updated_at").defaultTo(knex.fn.now());
+      })
+      .then(() => {
+        // Altera a colação da coluna 'title' após criar a tabela
+        return knex.schema.raw('ALTER TABLE notes MODIFY title VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;');
+      });
+    } else {
+      console.log('Tabela "notes" já existe, pulando a criação...');
+    }
   });
 };
 
