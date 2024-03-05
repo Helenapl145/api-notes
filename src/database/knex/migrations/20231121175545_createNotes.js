865f9deeb04f7ef1915776d4cc1f2a33e39ee359
Helena <helenapl145@gmail.com>
2024-03-05 17:07:43 -0300

    add migrate notes

--- src/database/knex/migrations/20231121175545_createNotes.js
+++ src/database/knex/migrations/20231121175545_createNotes.js
@@ -7,6 +7,10 @@ exports.up = knex => knex.schema.createTable("notes", table =>{
     table.integer("user_id").references("id").inTable("users");
     table.timestamp("created_at").default(knex.fn.now())
     table.timestamp("updated_at").default(knex.fn.now())
+    .then(() => {
+        // Altera a colação da coluna 'title' após criar a tabela
+        return knex.schema.raw('ALTER TABLE notes MODIFY title VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;');
+      });
 })
 
 
