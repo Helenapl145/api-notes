b599c5b2e1e384bafc2ed0652fd7c9ea98632b9a
Helena <helenapl145@gmail.com>
2024-03-05 17:11:22 -0300

    update createNotes

--- src/database/knex/migrations/20231121175545_createNotes.js
+++ src/database/knex/migrations/20231121175545_createNotes.js
@@ -1,17 +1,20 @@
-
-exports.up = knex => knex.schema.createTable("notes", table =>{
+exports.up = function(knex) {
+  return knex.schema.createTable("notes", table =>{
     table.increments("id");
 
-    table.text("title");
+    table.string("title");
     table.text("description");
     table.integer("user_id").references("id").inTable("users");
-    table.timestamp("created_at").default(knex.fn.now())
-    table.timestamp("updated_at").default(knex.fn.now())
-    .then(() => {
-        // Altera a colação da coluna 'title' após criar a tabela
-        return knex.schema.raw('ALTER TABLE notes MODIFY title VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;');
-      });
-})
+    table.timestamp("created_at").defaultTo(knex.fn.now()); // Correção aqui
+    table.timestamp("updated_at").defaultTo(knex.fn.now()); // Correção aqui
+  })
+  .then(() => {
+    // Altera a colação da coluna 'title' após criar a tabela
+    return knex.schema.raw('ALTER TABLE notes MODIFY title VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;');
+  });
+};
 
+exports.down = function(knex) {
+  return knex.schema.dropTable("notes");
+};
 
-exports.down = knex => knex.dropTable("notes")
